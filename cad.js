e609116fc38f4f59e33f5074bb7e525ab0e4af28
RickYeh <RickYeh@users.noreply.github.com>
2015-08-04 01:29:02 -0700

    Added insert object functions and click handlers

--- cad.js
+++ cad.js
@@ -19,17 +19,17 @@ function Shape(x, y, z, type) {
     this.type = type;
 }
 
-Shape.prototype.move = function(x, y, z) {
-    this.x += x;
-    this.y += y;
-    this.z += z;
+Shape.prototype.moveTo = function(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
 };
 
 Shape.prototype.toString = function() {
     return 'Shape(' + this.x + ' ' + this.y + ' ' + this.z + ')';
 };
 
-function Sphere(x, y, z, radius){
+function Sphere(x, y, z, radius) {
     Shape.call(this, x, y, z, SPHERE);
     this.radius = radius;
 }
@@ -57,14 +57,59 @@ Cone.prototype = new Shape();
 Cylinder.prototype = new Shape();
 Cube.prototype = new Shape();
 
-var a = new Sphere(0 , 0, 0, 10);
-var b = new Cone(0,0,0,5,3);
-var c = new Cylinder(0,0,0, 6, 8);
-var d = new Cube (0,0,0, 20);
+// Create object functions.  Will be called when button is clicked.
+function createSphere(x, y, z, r) {
+    objectsList.push(new Sphere(x, y, z, r));
+    console.log('Sphere created at (' + x + ',' + y + ',' + z + ') with r: ' + r);
+    updateObjectsList();
+}
+
+function createCone(x, y, z, r, h) {
+    objectsList.push(new Cone(x, y, z, r, h));
+    console.log('Cone created at (' + x + ',' + y + ',' + z + ') with r: ' + r + ' h: ' + h);
+    updateObjectsList();
+}
+
+function createCylinder(x, y, z, r, h) {
+    objectsList.push(new Cylinder(x, y, z, r, h));
+    console.log('Cylinder created at (' + x + ',' + y + ',' + z + ') with r: ' + r + ' h: ' + h);
+    updateObjectsList();
+}
+
+function createCube(x, y, z, s) {
+    objectsList.push(new Cube(x, y, z, s));
+    console.log('Cube created at (' + x + ',' + y + ',' + z + ') with s: ' + s);
+    updateObjectsList();
+}
+
+function updateObjectsList() {
+    console.log(objectsList);
+}
+
+// Assign click handlers
+$(document).ready(function() {
+    $('#insertSphere').click(function() {
+        createSphere(0, 0, 0, 5);
+    });
+    $('#insertCone').click(function() {
+        createCone(0, 0, 0, 5, 5);
+    });
+    $('#insertCylinder').click(function() {
+        createCylinder(0, 0, 0, 5, 5);
+    });
+    $('#insertCube').click(function() {
+        createCube(0, 0, 0, 5);
+    });
+});
+
+
+// Debugging
+var a = new Sphere(0, 0, 0, 10);
+var b = new Cone(0, 0, 0, 5, 3);
+var c = new Cylinder(0, 0, 0, 6, 8);
+var d = new Cube(0, 0, 0, 20);
 
 console.log(a);
 console.log(b);
 console.log(c);
 console.log(d);
-
-
