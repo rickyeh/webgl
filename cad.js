ae4b23566b263f4968e21f14a0eef4a760638b82
RickYeh <RickYeh@users.noreply.github.com>
2015-08-03 23:47:57 -0700

    fixed typos and bugs

--- cad.js
+++ cad.js
@@ -1,16 +1,17 @@
+//Shape.prototype.toString = function() { return 'Shape( ' + this.x + ' ' + this.y +')' } 
+
+
+// CONSTANTS
 var SPHERE = 'sphere';
 var CONE = 'cone';
 var CYLINDER = 'cylinder';
 var CUBE = 'cube';
 
-var createShapes = {
+// Main array to store the created objects
+var objectsList = [];
 
-};
 
-// Sphere.prototype = new Shape('sphere', center);
-
-//Shape.prototype.toString = function() { return 'Shape( ' + this.x + ' ' + this.y +')' } 
-
+// Declare objects
 function Shape(x, y, z, type) {
     this.x = x;
     this.y = y;
@@ -24,27 +25,36 @@ function Sphere(x, y, z, radius){
 }
 
 function Cone(x, y, z, radius, height) {
-    Shape.call(this. x, y, z, CONE);
+    Shape.call(this, x, y, z, CONE);
     this.radius = radius;
     this.height = height;
 }
 
 function Cylinder(x, y, z, radius, height) {
-    Shape.call(this. x, y, z, CYLINDER);
+    Shape.call(this, x, y, z, CYLINDER);
     this.radius = radius;
     this.height = height;
 }
 
 function Cube(x, y, z, side) {
-    Shape.call(this. x, y, z, CUBE);
+    Shape.call(this, x, y, z, CUBE);
     this.side = side;
 }
 
+// Set up prototype chains
 Sphere.prototype = new Shape();
 Cone.prototype = new Shape();
 Cylinder.prototype = new Shape();
 Cube.prototype = new Shape();
 
+var a = new Sphere(0 , 0, 0, 10);
+var b = new Cone(0,0,0,5,3);
+var c = new Cylinder(0,0,0, 6, 8);
+var d = new Cube (0,0,0, 20);
 
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
 
 
